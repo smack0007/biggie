386d13c9206659b8e5780d5af8a39eefd80385ad
Zachary Snow <zachary.snow@gmail.com>
2025-06-19 11:21:58 +0200

    Switch back to using int32.

--- src/runtime/biggie.cpp
+++ src/runtime/biggie.cpp
@@ -7,16 +7,16 @@
 #define _CONCAT(a, b) a##b
 #define CONCAT(a, b) _CONCAT(a, b)
 
-typedef float_t f32;
-typedef double_t f64;
-typedef int8_t i8;
-typedef int16_t i16;
-typedef int32_t i32;
-typedef int64_t i64;
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+typedef float_t float32;
+typedef double_t float64;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
 typedef std::string string;
 
